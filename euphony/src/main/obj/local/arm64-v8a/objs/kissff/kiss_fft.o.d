c1747a9fd62f9bcc6c099a6d49104b7357565f97
Ji-woong Choi <hello.designe@facebook.com>
2019-07-11 08:56:15 +0900

    ver 0.7.1.4 / added demo android app for listener

---
+++ euphony/src/main/obj/local/arm64-v8a/objs/kissff/kiss_fft.o.d
@@ -0,0 +1,8 @@
+/Users/jbear/AndroidStudioProjects/Euphony/euphony/src/main/obj/local/arm64-v8a/objs/kissff/kiss_fft.o: \
+  /Users/jbear/AndroidStudioProjects/Euphony/euphony/src/main/jni/kiss_fft.c \
+  /Users/jbear/AndroidStudioProjects/Euphony/euphony/src/main/jni/_kiss_fft_guts.h \
+  /Users/jbear/AndroidStudioProjects/Euphony/euphony/src/main/jni/kiss_fft.h
+
+/Users/jbear/AndroidStudioProjects/Euphony/euphony/src/main/jni/_kiss_fft_guts.h:
+
+/Users/jbear/AndroidStudioProjects/Euphony/euphony/src/main/jni/kiss_fft.h:
