198e52faa147b4d6472363370d2302cadef1928d
Ji-woong Choi <hello.designe@facebook.com>
2019-07-19 23:17:10 +0900

    Update some example

--- assets/js/main.js
+++ assets/js/main.js
@@ -5,12 +5,14 @@ function generateSound() {
     if(btnSwt) {
         euphy.stop();
         document.getElementById("euphy_btn").value = "broadcast";
+        btnSwt = false;
     }
     else {
         var code = document.getElementById("euphy_text");
         euphy.setCode(code.value);
         euphy.play();
         document.getElementById("euphy_btn").value = "stop";
+        btnSwt = true;
     }
 
 
