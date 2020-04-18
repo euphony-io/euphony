ec1d46918f5e11d23b4c54dae3f482048ad5abd3
designe <hello.designe@facebook.com>
2020-04-18 15:20:55 +0900

    euphony.js 0.2.2 is applied

--- research/euphony-research.js
+++ research/euphony-research.js
@@ -2,10 +2,16 @@ var euphy = new Euphony();
 
 function setFrequency(e) {
     $("#frequency-viewer .view-panel").text(e.value + " hz");
+    euphy.initBuffers();
+    euphy.setFrequency(e.value);
 }
 
 function setVolume(e) {
-    $("#volume-viewer .view-panel").text(e.value + "/ 100");
+    $("#volume-viewer .view-panel").text(e.value + " / 100");
+}
+
+function onPlayBtn() {
+    euphy.play(false);
 }
 
 $(document).ready(function() {
