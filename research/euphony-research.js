23a270bd20d322cc717cbe8ff048bc4358b1583f
designe <hello.designe@facebook.com>
2020-04-23 11:34:16 +0900

    research page's firefox working version

--- research/euphony-research.js
+++ research/euphony-research.js
@@ -7,20 +7,40 @@ function setFrequency(e) {
 }
 
 function setVolume(e) {
+    console.log("setVolume : " + e.value);
     $("#volume-viewer .view-panel").text(e.value + " / 100");
+    euphy.setVolume(e.value);
 }
 
 function onPlayBtn() {
-    if(euphy.getState() == 0){
+    let freq = $("#frequencyRange").val();
+    console.log(euphy.getState());
+    switch(euphy.getState()) {
+    case 0: // STOP 2 PLAY
+        console.log("STOP 2 PLAY : " + freq);
+        $("#play-btn").text("PAUSE");
+        euphy.initBuffers();
+        euphy.setFrequency(freq);
         euphy.play(false);
-        $("#play-btn").text("STOP");
-    }
-    else {
-        euphy.stop();
+        break;
+    case 1: // PLAY 2 PAUSE
+        console.log("PLAY 2 PAUSE : ");
         $("#play-btn").text("PLAY");
+        euphy.pause();
+        break;
+    case 2: // PAUSE 2 PLAY
+        console.log("PAUSE 2 PLAY : " + freq);
+        $("#play-btn").text("PAUSE");
+        euphy.setFrequency(freq);
+        euphy.play(false);
+        break;
     }
 }
 
+function onStopBtn() {
+    $("#play-btn").text("PLAY");
+    euphy.stop();
+}
 $(document).ready(function() {
     setFrequency({ value : 400 });
     setVolume({ value : 50 });
