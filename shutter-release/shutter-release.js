a863875488a133e8087cc6d05eeacc33e031c17b
designe <hello.designe@facebook.com>
2020-06-02 17:54:53 +0900

    added wakashubu service

---
+++ shutter-release/shutter-release.js
@@ -0,0 +1,24 @@
+var euphyShutter = new Euphony();
+
+function shutterStart() {
+
+}
+
+$(document).ready(function() {
+    $("#shutter-button").bind("touchstart, mousedown", function(e) {
+        e.preventDefault();
+        euphyShutter.initBuffers();
+        euphyShutter.setFrequency(19500);
+        euphyShutter.setVolume(100);
+        euphyShutter.play();        
+    });
+
+    $("#shutter-button").bind("touchend, mouseup", function(e) {
+        e.preventDefault();
+        euphyShutter.pause();
+    });
+
+
+});
+
+    
