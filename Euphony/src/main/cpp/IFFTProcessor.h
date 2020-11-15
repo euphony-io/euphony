7dc30f5f22d8556e277c305d086d717c3c8c0d32
designe <hello.designe@facebook.com>
2020-11-15 23:57:38 +0900

    Added IFFTProcessor for Euphony FFT Interface

---
+++ Euphony/src/main/cpp/IFFTProcessor.h
@@ -0,0 +1,16 @@
+//
+// Created by desig on 2020-11-15.
+//
+
+#ifndef EUPHONY_IFFTPROCESSOR_H
+#define EUPHONY_IFFTPROCESSOR_H
+
+class IFFTProcessor {
+public:
+    virtual void create() = 0;
+    virtual void destroy() = 0;
+    virtual void doSpectrum() = 0;
+    virtual void doSpectrums() = 0;
+};
+
+#endif //EUPHONY_IFFTPROCESSOR_H
