518c38eb6f1cbb990807b2a49466bdc596b5c266
designe <hello.designe@facebook.com>
2021-09-13 19:37:13 +0900

    Made PlayerEngine Concept > ANDROID_DEFAULT_ENGINE : it is using AudioTrack class (Android API). that was using on v0.7.1.6 > EUPHONY_NATIVE_ENGINE : it is applied on v0.8. there is some amplitude issue. need to be constantly updated. Default value is ANDROID_DEFAULT_ENGINE. because EUPHONY_NATIVE_ENGINE has some amplitude issue. it is experimental.

--- euphony/src/main/java/co/euphony/tx/EuTxManager.java
+++ euphony/src/main/java/co/euphony/tx/EuTxManager.java
@@ -1,18 +1,33 @@
 package co.euphony.tx;
 
 import android.content.Context;
+import android.media.AudioFormat;
+import android.media.AudioManager;
+import android.media.AudioTrack;
 import android.os.Handler;
 import android.os.Looper;
+import android.util.Log;
 
 import co.euphony.util.EuOption;
 
+import static android.media.AudioTrack.SUCCESS;
+import static android.media.AudioTrack.WRITE_BLOCKING;
+
 public class EuTxManager {
 	private EuTxNativeConnector txCore;
+	private AudioTrack mAudioTrack = null;
+	private EuOption.ModeType modeType;
+	private PlayerEngine playerEngineType;
 
 	public enum EuPIDuration {
 		LENGTH_SHORT,
 		LENGTH_LONG,
 		LENGTH_FOREVER
+	}
+
+	public enum PlayerEngine {
+		ANDROID_DEFAULT_ENGINE,
+		EUPHONY_NATIVE_ENGINE
 	}
 
 	public EuTxManager(Context context) {
@@ -37,7 +52,7 @@ public class EuTxManager {
 	}
 
 	public void callEuPI(double freq, EuPIDuration duration) {
-		txCore.setMode(EuOption.ModeType.EUPI);
+		setMode(EuOption.ModeType.EUPI);
 		txCore.setToneOn(true);
 		txCore.setAudioFrequency(freq);
 		txCore.start();
@@ -48,46 +63,95 @@ public class EuTxManager {
 		}
 	}
 
+	public float[] getOutStream() {
+		return txCore.getGenWaveSource();
+	}
+
 	public String getGenCode() {
 		return txCore.getGenCode();
 	}
 
-	public short[] getOutStream() {
-
-		/* TODO: legacy code will be removed.
-		    That will be bring from EuphonyTx.
-		return mOutStream;
-		 */
-		return null;
-	}
-
 	public void play() {
-		play(1);
+		play(1, PlayerEngine.ANDROID_DEFAULT_ENGINE);
 	}
 
-	public void play(int count) {
-		txCore.setMode(EuOption.ModeType.DEFAULT);
+	public void play(final int count) {
+		play(count, PlayerEngine.ANDROID_DEFAULT_ENGINE);
+	}
+
+	public void play(final int count, PlayerEngine engineType) {
+		playerEngineType = engineType;
+		setMode(EuOption.ModeType.DEFAULT);
+		if(engineType == PlayerEngine.EUPHONY_NATIVE_ENGINE) {
+			playWithNativeEngine(count);
+		} else {
+			playWithAndroidEngine(count);
+		}
+	}
+
+	private void playWithNativeEngine(final int count) {
 		txCore.setCountToneOn(true, count);
 		txCore.start();
 	}
 
-	/*
-	 * @deprecated Replaced by {@link #setCode()}, deprecated for naming issue
-	 */
-	@Deprecated
-	public void process() { play(1); }
+	short[] outShortStream;
+	private void playWithAndroidEngine(int count) {
+		float[] outStream = txCore.getGenWaveSource();
+
+		mAudioTrack = new AudioTrack(AudioManager.STREAM_MUSIC, 44100, AudioFormat.CHANNEL_OUT_MONO, AudioFormat.ENCODING_PCM_16BIT, outStream.length*2, AudioTrack.MODE_STATIC);
+		if(count <= 0)
+			count = -1;
+
+		int result = mAudioTrack.setLoopPoints(0, outStream.length, count);
+		if(result != SUCCESS) {
+			Log.i("PROCESS", "failed to loop points : " + result);
+		}
+
+		outShortStream = new short[outStream.length];
+		for(int i = 0; i < outStream.length; i++) {
+			outShortStream[i] = (short) (32767 * outStream[i]);
+		}
+
+		if(mAudioTrack != null){
+			try{
+				mAudioTrack.write(outShortStream, 0, outShortStream.length);
+				mAudioTrack.play();
+			}
+			catch(IllegalStateException e)
+			{
+				Log.i("PROCESS", e.getMessage());
+			}
+		}
+	}
+
+	public void setMode(EuOption.ModeType modeType) {
+		this.modeType = modeType;
+		txCore.setMode(modeType);
+	}
 
 	/*
 	 * @deprecated Replaced by {@link #setCode()}, deprecated for naming issue
 	 */
 	@Deprecated
-	public void process(int count) { play(count); }
+	public void process() { play(1, PlayerEngine.ANDROID_DEFAULT_ENGINE); }
+
+	/*
+	 * @deprecated Replaced by {@link #setCode()}, deprecated for naming issue
+	 */
+	@Deprecated
+	public void process(int count) { play(count, PlayerEngine.ANDROID_DEFAULT_ENGINE); }
 
 	public void stop()
 	{
-		txCore.setToneOn(false);
-		new Handler(Looper.getMainLooper()).postDelayed(() -> {
-			txCore.stop();
-		},300);
+		if(modeType == EuOption.ModeType.DEFAULT && playerEngineType == PlayerEngine.ANDROID_DEFAULT_ENGINE) {
+			if(mAudioTrack != null)
+				mAudioTrack.pause();
+		}
+		else {
+			txCore.setToneOn(false);
+			new Handler(Looper.getMainLooper()).postDelayed(() -> {
+				txCore.stop();
+			}, 300);
+		}
 	}
 }
