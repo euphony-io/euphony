a1ddc0eb6db8be451d0b30e54ff121c3a78cb47b
designe <hello.designe@facebook.com>
2021-09-13 10:13:49 +0900

    Remove Position Detector > it doesn't no more support. > `Distance positioning` function will be available on v1.0

--- euphony/src/main/java/co/euphony/rx/EuRxManager.java
+++ euphony/src/main/java/co/euphony/rx/EuRxManager.java
@@ -8,12 +8,9 @@ import android.util.Log;
 public class EuRxManager {
 	private Thread mRxThread = null;
 	private RxRunner mRxRunner = null;
-	private PsRunner mPsRunner = null;
-	private Thread mPsThread = null;
 	private boolean _active;
 	
 	private static final int RX_DECODE = 1;
-	private static final int PS_DECODE = 2;
 
 	private boolean mHex = false;
 	public EuRxManager() { }
@@ -29,35 +26,6 @@ public class EuRxManager {
 		mRxRunner = new RxRunner(hex);
 		mRxThread = new Thread(mRxRunner, "RX");
 		mRxThread.start();
-	}
-	
-	public void find() 
-	{
-		_active = true;
-		mPsRunner = new PsRunner();
-		mPsThread = new Thread(mPsRunner, "PS");
-		mPsThread.start();
-	}
-	
-	public void finishToFind()
-	{
-		if(mPsThread != null) {
-			_active = false;
-			while (true) {
-				try {
-					mPsThread.join();
-					break;
-				} catch (InterruptedException e) {
-					Log.i("FINISH", e.getMessage());
-				}
-			}
-		}
-
-		if(mPsRunner != null)
-			mPsRunner.destroyFFT();
-
-		mPsThread = null;
-		mPsRunner = null;
 	}
 	
 	public void finish()
@@ -101,27 +69,7 @@ public class EuRxManager {
 			}
 		}
 	};
-	
-	private PositionDetector mPositionDetector;
-	
-	public PositionDetector getPositionDetector() {
-		return mPositionDetector;
-	}
-	
-	public void setPositionDetector(PositionDetector detector) {
-		this.mPositionDetector = detector;
-	}
-	
-	private Handler mPsHandler = new Handler() {
-		public void handleMessage(Message msg) {
-			switch(msg.what){
-				case PS_DECODE:
-					mPositionDetector.detectSignal((Integer)msg.obj);
-					break;
-			}
-		} 
-	};
-	
+
 	private class RxRunner extends EuFreqObject implements Runnable{
 		boolean mHex = false;
 		RxRunner() { }
@@ -149,74 +97,6 @@ public class EuRxManager {
 					return;
 				}
 			}		
-		}
-	}
-	
-	private class PsRunner extends EuFreqObject implements Runnable {
-
-		@Override
-		public void run() {
-			boolean startswt = false;
-			int startcnt = 0;
-			int specificFreq = 0;
-			Log.i("START", "START LISTEN");
-			while(_active) {
-				//To find the frequency point
-				while(!startswt) {
-					processFFT();
-					int i;
-					for(i = 21000; i >= 16500; i-= Constants.CHANNEL_SPAN)
-						if(100 < detectFreq(i)){
-							startswt = true;
-							break;
-						}
-					specificFreq = i;
-					
-					//there is no af area..
-					if(startcnt++ > 1000){
-						_active = false;
-						startswt = true;
-						Log.i("START", "FAILED to find any position");
-					}
-				}
-				
-				int signal, max_signal = 0, avr_signal = 0;
-				int noSignalCnt=0, processingCnt = 0, maxCnt=0;
-				do{
-					processFFT();
-					signal = detectFreq(specificFreq);
-					
-					if(signal < 20)
-						noSignalCnt++;
-					else{
-						noSignalCnt = 0;
-						
-						if(max_signal < signal){
-							maxCnt++;
-							max_signal = signal;
-							avr_signal += max_signal;
-						}
-						if(++processingCnt > 50){
-							avr_signal /= maxCnt;
-							Message msg = mPsHandler.obtainMessage();
-							msg.what = PS_DECODE;
-							msg.obj = avr_signal;
-							mPsHandler.sendMessage(msg);
-							processingCnt = 0;
-							max_signal = 0;
-							avr_signal = 0;
-							maxCnt = 0;
-						}
-					}
-				}while(noSignalCnt < 50 && _active);
-				
-				Message msg = mPsHandler.obtainMessage();
-				msg.what = PS_DECODE;
-				msg.obj = -1;
-				mPsHandler.sendMessage(msg);
-				break;
-				
-			}
 		}
 	}
 }
--- euphony/src/main/java/co/euphony/rx/PositionDetector.java
+++
@@ -1,6 +0,0 @@
-package co.euphony.rx;
-
-public interface PositionDetector {
-	void detectSignal(int signal);
-	void detectFq(int fq);
-}
