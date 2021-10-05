53c605f178d69a46ebaaffef58d9527c847fc31f
aiclaudev <88221233+aiclaudev@users.noreply.github.com>
2021-10-05 15:42:39 +0900

    Create UTF8Charset to support Korean language
    Co-Authored-By: SangYeon Min <judemin2087@naver.com>
    Co-Authored-By: JK Park <kuro11pow2@gmail.com>

---
+++ euphony/src/main/cpp/core/source/UTF8Charset.cpp
@@ -0,0 +1,26 @@
+#include "UTF8Charset.h"
+
+#include <iostream>
+#include <string>
+
+using namespace Euphony;
+
+HexVector UTF8Charset::encode(std::string src) {
+    HexVector result = HexVector(src.size());
+
+    for (int i = 0; i < src.size(); i++)
+        result.pushBack(src[i]);
+
+    return result;
+}
+
+std::string UTF8Charset::decode(const HexVector& src) {
+    std::string result;
+    std::vector<u_int8_t> hexSource = src.getHexSource();
+
+    for (int i = 0; i < hexSource.size() / 2; i++)
+        result += (hexSource[2 * i] << 4) | hexSource[2 * i + 1];
+
+    return result;
+}
+
