739d021885c67dbf79e31ba55897af99abe9af84
kuro11pow2 <kuro11pow2@gmail.com>
2021-10-07 21:24:15 +0900

    Simplify UTF16Charset
    Co-Authored-By: designe <designe@users.noreply.github.com>
    Co-Authored-By: ycs1m1yk <satoly4@gmail.com>

--- euphony/src/main/cpp/core/UTF16Charset.h
+++ euphony/src/main/cpp/core/UTF16Charset.h
@@ -11,6 +11,11 @@ namespace Euphony {
         ~UTF16Charset() = default;
         HexVector encode(std::string src);
         std::string decode(const HexVector &src);
+    private:
+        const u_int8_t BIT_COUNT_IN_HEX_NUM = 4;
+        const u_int8_t BYTE_COUNT = 2;
+        const u_int8_t HEX_NUM_COUNT = BYTE_COUNT * 8 / BIT_COUNT_IN_HEX_NUM;
+        const char16_t BIT_MASK = 0x000F;
     };
 }
 
--- euphony/src/main/cpp/core/source/UTF16Charset.cpp
+++ euphony/src/main/cpp/core/source/UTF16Charset.cpp
@@ -1,34 +1,22 @@
-#include "UTF16Charset.h"
-#include <bitset>
+#include "../UTF16Charset.h"
 #include <codecvt>
 #include <iomanip>
 
 using namespace Euphony;
-
-const unsigned int BIT_COUNT_IN_HEX_NUM = 4;
-const unsigned int BYTE_COUNT = 2;
-const unsigned int HEX_NUM_COUNT = BYTE_COUNT * 8 / BIT_COUNT_IN_HEX_NUM;
 
 HexVector UTF16Charset::encode(std::string src) {
     std::wstring_convert<std::codecvt_utf8_utf16<char16_t>, char16_t> convert;
     std::u16string utf16s = convert.from_bytes(src);
     HexVector result = HexVector(2 * utf16s.size());
 
-    for (int char_idx = 0; char_idx < utf16s.size(); char_idx++) {
-        char16_t ch = utf16s[char_idx];
-        std::bitset<16> char_bits = std::bitset<16>(ch);
-        std::vector<u_int8_t> tmp;
-        for (int hex_idx = 0; hex_idx < HEX_NUM_COUNT; hex_idx++) {
-            u_int8_t hex_num = 0;
-            for (int bit_idx = 0; bit_idx < BIT_COUNT_IN_HEX_NUM; bit_idx++) {
-                if (char_bits.test(BIT_COUNT_IN_HEX_NUM * hex_idx + bit_idx)) {
-                    hex_num += 1 << bit_idx;
-                }
-            }
-            tmp.push_back(hex_num);
+    for (char16_t ch : utf16s) {
+        std::vector<u_int8_t> halfByteBuffer;
+        for(int hexIdx = 0; hexIdx < HEX_NUM_COUNT; hexIdx++) {
+            halfByteBuffer.push_back(ch & BIT_MASK);
+            ch >>= BIT_COUNT_IN_HEX_NUM;
         }
-        for (auto iter = tmp.rbegin(); iter != tmp.rend(); iter++) {
-            result.pushBack(*iter);
+        for (auto it = halfByteBuffer.rbegin(); it != halfByteBuffer.rend(); it++) {
+            result.pushBack(*it);
         }
     }
 
@@ -41,13 +29,11 @@ std::string UTF16Charset::decode(const HexVector &src) {
     std::string result = "";
     std::vector<u_int8_t> hexSource = src.getHexSource();
 
-    for (int hex_idx = 0; hex_idx < hexSource.size() / HEX_NUM_COUNT; hex_idx++) {
-        char16_t ch = 0;
-        for (int bit_idx = 0; bit_idx < BIT_COUNT_IN_HEX_NUM; bit_idx++) {
-            u_int8_t n = hexSource[BIT_COUNT_IN_HEX_NUM * hex_idx + bit_idx];
-            u_int offset = (BYTE_COUNT * 8 - (bit_idx + 1) * BIT_COUNT_IN_HEX_NUM);
-            ch += n << offset;
-        }
+    for (int hexIdx = 0; hexIdx < hexSource.size(); hexIdx += HEX_NUM_COUNT) {
+        char16_t ch = hexSource[hexIdx];
+        ch = (ch << BIT_COUNT_IN_HEX_NUM) | hexSource[hexIdx + 1];
+        ch = (ch << BIT_COUNT_IN_HEX_NUM) | hexSource[hexIdx + 2];
+        ch = (ch << BIT_COUNT_IN_HEX_NUM) | hexSource[hexIdx + 3];
         utf16s += ch;
     }
 
