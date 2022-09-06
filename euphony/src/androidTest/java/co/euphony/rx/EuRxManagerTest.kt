9bb0954cd78e7995f9f1d2eaf00a1d3c8640934a
sunyeongan <asy100@naver.com>
2022-09-06 17:28:34 +0900

    Add advanced logs to call listen() without permission use try-catch statements (#205) Co-authored-by: zion830 <zion830@naver.com>

--- euphony/src/androidTest/java/co/euphony/rx/EuRxManagerTest.kt
+++ euphony/src/androidTest/java/co/euphony/rx/EuRxManagerTest.kt
@@ -6,13 +6,10 @@ import org.junit.runner.RunWith
 @RunWith(AndroidJUnit4::class)
 class EuRxManagerTest {
 
-    @Test
-    public fun testListen() {
-
-        @Test(expected = Test.None::class) /* no exception expected */
-        fun withExceptionHandlingNoErrorOccurs() {
-            val rxManager = EuRxManager()
-            rxManager.listen()
-        }
+    @Test(expected = Test.None::class) /* no exception expected */
+    fun withExceptionHandlingNoErrorOccurs() {
+        val rxManager = EuRxManager()
+        rxManager.listen()
     }
+
 }
