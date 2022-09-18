de08caf89d15200e8bd57b74c67902cf94c00d92
SANGHYUN KIM <haan823@naver.com>
2022-09-18 22:20:45 +0900

    test: EuphonyRxPanel ui test

---
+++ euphony/src/androidTest/java/co/euphony/ui/EuphonyRxPanelTest.kt
@@ -0,0 +1,114 @@
+package co.euphony.ui
+
+import androidx.compose.material.ExperimentalMaterialApi
+import androidx.compose.ui.ExperimentalComposeUiApi
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.PixelMap
+import androidx.compose.ui.test.*
+import androidx.compose.ui.test.junit4.createComposeRule
+import co.euphony.common.Constants
+import co.euphony.ui.theme.LightGreen
+import co.euphony.ui.theme.LightRed
+import co.euphony.ui.theme.LightSkyBlue
+import co.euphony.ui.viewmodel.EuphonyRxPanelViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import org.junit.Before
+import org.junit.Rule
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.mockito.Mock
+import org.mockito.Mockito
+import org.mockito.junit.MockitoJUnitRunner
+
+@ExperimentalMaterialApi
+@ExperimentalComposeUiApi
+@RunWith(MockitoJUnitRunner::class)
+class EuphonyRxPanelTest {
+
+    @get:Rule
+    val composeTestRule = createComposeRule()
+
+    @Mock
+    private lateinit var viewModel: EuphonyRxPanelViewModel
+
+    private val isListening = MutableStateFlow(false)
+    private val isListenStarted = MutableStateFlow(false)
+    private val rxCode = MutableStateFlow("")
+
+    @Before
+    fun setup() {
+        Mockito.`when`(viewModel.isListening).thenReturn(isListening)
+        Mockito.`when`(viewModel.isListenStarted).thenReturn(isListenStarted)
+        Mockito.`when`(viewModel.rxCode).thenReturn(rxCode)
+
+        composeTestRule.setContent {
+            EuphonyRxPanelImpl(viewModel = viewModel)
+        }
+    }
+
+    @Test
+    fun initEuphonyRxPanel() {
+        composeTestRule.onNodeWithTag(Constants.LISTEN_TAG_BUTTON)
+            .assertTextContains(Constants.LISTEN_BUTTON)
+        composeTestRule.onNodeWithTag(Constants.LISTEN_TAG_OUTPUT).assertIsNotEnabled()
+        composeTestRule.onNodeWithTag(Constants.LISTEN_TAG_OUTPUT).assertContainsColor(LightSkyBlue)
+
+    }
+
+    @Test
+    fun startEuphonyRxPanelAndReturnSuccess() {
+        composeTestRule.onNodeWithTag(Constants.LISTEN_TAG_BUTTON).performClick()
+
+        processEuphonyRxPanel()
+        composeTestRule.onNode(hasText(Constants.LISTEN_PROGRESS_BUTTON)).assertIsDisplayed()
+
+        rxCode.value = "data"
+
+        doneEuphonyRxPanel()
+        Mockito.`when`(viewModel.isSuccess()).thenReturn(true)
+
+        composeTestRule.onNodeWithTag(Constants.LISTEN_TAG_BUTTON)
+            .assertTextContains(Constants.LISTEN_BUTTON)
+        composeTestRule.onNodeWithTag(Constants.LISTEN_TAG_OUTPUT).assertContainsColor(LightGreen)
+        composeTestRule.onNodeWithTag(Constants.LISTEN_TAG_OUTPUT).assertTextContains("data")
+    }
+
+    @Test
+    fun startEuphonyRxPanelAndReturnFail() {
+        composeTestRule.onNodeWithTag(Constants.LISTEN_TAG_BUTTON).performClick()
+
+        processEuphonyRxPanel()
+        composeTestRule.onNode(hasText(Constants.LISTEN_PROGRESS_BUTTON)).assertIsDisplayed()
+
+        doneEuphonyRxPanel()
+        Mockito.`when`(viewModel.isSuccess()).thenReturn(false)
+
+        composeTestRule.onNodeWithTag(Constants.LISTEN_TAG_BUTTON)
+            .assertTextContains(Constants.LISTEN_BUTTON)
+        composeTestRule.onNodeWithTag(Constants.LISTEN_TAG_OUTPUT).assertContainsColor(LightRed)
+        composeTestRule.onNodeWithTag(Constants.LISTEN_TAG_OUTPUT).assertTextContains("")
+    }
+
+    private fun processEuphonyRxPanel() {
+        isListenStarted.value = true
+        isListening.value = true
+    }
+
+    private fun doneEuphonyRxPanel() {
+        isListening.value = false
+    }
+
+    private fun SemanticsNodeInteraction.assertContainsColor(tint: Color): SemanticsNodeInteraction {
+        val imageBitmap = captureToImage()
+        val buffer = IntArray(imageBitmap.width * imageBitmap.height)
+        imageBitmap.readPixels(buffer, 0, 0, imageBitmap.width - 1, imageBitmap.height - 1)
+        val pixelColors = PixelMap(buffer, 0, 0, imageBitmap.width - 1, imageBitmap.height - 1)
+
+        (0 until imageBitmap.width).forEach { x ->
+            (0 until imageBitmap.height).forEach { y ->
+                if (pixelColors[x, y] == tint) return this
+            }
+        }
+        throw AssertionError("Assert failed: The component does not contain the color")
+    }
+}
